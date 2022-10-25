4603c363c0bb29060285d6190de7f3357e3625be
Joshi Bhavya <jshi.bhavya@gmail.com>
2022-10-26 00:57:37 +0530

    minor bug fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const environment = {
   production: true,
   API_URI:"https://myresume-bhavyajsh.herokuapp.com/api/",
   ADMIN_URI:"https://myresume-bhavyajsh.herokuapp.com/sudouser/" 
-};
+}; 
