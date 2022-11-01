8e1496ebbc790adfd7f8cf59c562d72801f1b95a
Joshi Bhavya <jshi.bhavya@gmail.com>
2022-11-01 12:38:43 +0530

    Transfer to new cloud

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  API_URI:"https://myresume-bhavyajsh.herokuapp.com/api/",
-  ADMIN_URI:"https://myresume-bhavyajsh.herokuapp.com/sudouser/" 
+  // API_URI:"https://myresume-bhavyajsh.herokuapp.com/api/",
+  // ADMIN_URI:"https://myresume-bhavyajsh.herokuapp.com/sudouser/"
+  API_URI:"https://courageous-tunic-worm.cyclic.app/api/",
+  ADMIN_URI:"https://courageous-tunic-worm.cyclic.app/sudouser/"
+   
 }; 
