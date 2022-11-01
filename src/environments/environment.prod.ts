a9be4890e5642401625563478dde67de05f73d7c
Joshi Bhavya <jshi.bhavya@gmail.com>
2022-11-02 00:50:35 +0530

    Rollback

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  // API_URI:"https://myresume-bhavyajsh.herokuapp.com/api/",
-  // ADMIN_URI:"https://myresume-bhavyajsh.herokuapp.com/sudouser/"
-  API_URI:"https://courageous-tunic-worm.cyclic.app/api/",
-  ADMIN_URI:"https://courageous-tunic-worm.cyclic.app/sudouser/"
+  API_URI:"https://myresume-bhavyajsh.herokuapp.com/api/",
+  ADMIN_URI:"https://myresume-bhavyajsh.herokuapp.com/sudouser/"
+
    
 }; 
