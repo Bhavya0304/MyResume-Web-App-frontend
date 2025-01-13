cab3ea2a7f90d955fa3e3a7896a37918c8ee33e3
Bhavya Joshi <jshi.bhavya@gmail.com>
2025-01-13 13:29:53 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // API_URI:"https://myresume-bhavyajsh.herokuapp.com/api/",
   // ADMIN_URI:"https://myresume-bhavyajsh.herokuapp.com/sudouser/",
-  API_URI:"https://bhavyajoshi.work.gd/api/",
-  ADMIN_URI:"https://bhavyajoshi.work.gd/sudouser/"
+  API_URI:"https://joshibhavya.com/api/",
+  ADMIN_URI:"https://joshibhavya.com/sudouser/"
 }; 
